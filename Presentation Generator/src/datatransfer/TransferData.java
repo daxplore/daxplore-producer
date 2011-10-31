893ca6717f456192cb4ec966c5b10745ee7506f2
Daniel Dunér <daniel.duner@gmail.com>
2011-10-31 19:50:45 +0100

    Renamed and moved xerial/XerialTest to datatransfer/TransferData.

--- Presentation Generator/src/datatransfer/TransferData.java
+++ Presentation Generator/src/datatransfer/TransferData.java
@@ -1,4 +1,4 @@
-package xerial;
+package datatransfer;
 
 import java.io.File;
 import java.sql.Connection;
@@ -10,9 +10,8 @@ import java.sql.Statement;
 
 import StatSafty.NaiveBayesClassifier;
 
-import datatransfer.SPSStoSQL;
 
-public class XerialTest {
+public class TransferData {
 	public static void main(String[] args) throws ClassNotFoundException {
 		// load the sqlite-JDBC driver using the current class loader
 		Class.forName("org.sqlite.JDBC");
@@ -26,35 +25,11 @@ public class XerialTest {
 		try {
 			// create a database connection
 			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-			//connection = DriverManager.getConnection("jdbc:sqlite::memory:");
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(30); // set timeout to 30 sec.
 
 			statement.executeUpdate("drop table if exists person");
-			/*statement.executeUpdate("create table person (id integer, name string)");
-			statement.executeUpdate("insert into person values(1, 'leo')");
-			statement.executeUpdate("insert into person values(2, 'yui')");
-			
-			PreparedStatement prepared = connection.prepareStatement("insert into person values(?, ?)");
-			
-			prepared.setInt(1, 3);
-			prepared.setString(2, "dan");
-			prepared.execute();
-			//Integer a = null;
-			//prepared.setInt(1, a);
-			prepared.setString(2, "the man");
-			prepared.execute();
-			
-			prepared.setInt(1, 5);
-			prepared.setString(2, "åäöĉäá");
-			prepared.execute();
-			
-			ResultSet rs = statement.executeQuery("select * from person");
-			while (rs.next()) {
-				// read the result set
-				System.out.println("name = " + rs.getString("name"));
-				System.out.println("id = " + rs.getInt("id"));
-			}*/
+
 			if(args.length >0){
 				spssFileName = args[0];
 				File spssFile = new File(spssFileName);
