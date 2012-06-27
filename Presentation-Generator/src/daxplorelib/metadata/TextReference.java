620bb75c73c823848fbbbf9dbf1bedb781e849b8
synthax <synthax@gmail.com>
2012-06-27 15:21:48 +0200

    TextReference implementation finnished(?)

--- Presentation-Generator/src/daxplorelib/metadata/TextReference.java
+++ Presentation-Generator/src/daxplorelib/metadata/TextReference.java
@@ -1,25 +1,63 @@
 package daxplorelib.metadata;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Locale;
 
 import org.json.simple.JSONAware;
 
 public class TextReference implements JSONAware, Comparable<TextReference> {
-	String reference;
+	protected static final String sqlDefinition = "CREATE TABLE texts (ref TEXT, locale TEXT, text TEXT, UNIQUE ( ref, locale) )";
 	
-	public TextReference(String reference){
+	String reference;
+	Connection connection;
+	
+	/**
+	 * TextReference is a identifier for strings. It supports localization and generally works like a map.
+	 * @param reference The string reference
+	 * @param connection Database connection (SQLite may be expected)
+	 */
+	public TextReference(String reference, Connection connection) {
 		this.reference = reference;
+		this.connection = connection;
 	}
 	
 	public String getRef() {
 		return reference;
 	}
 	
-	public String get(Locale locale){
+	public String get(Locale locale) throws SQLException, Exception {
+		PreparedStatement stmt = connection.prepareStatement("SELECT text FROM texts WHERE ref = ? AND locale = ?");
+		stmt.setString(1, reference);
+		stmt.setString(2, locale.toLanguageTag());
+		stmt.execute();
+		ResultSet rs = stmt.getResultSet();
+		if(rs.next()) {
+			String text = rs.getString("text");
+			if(rs.next()) {
+				throw new Exception("more than one result");
+			}
+			return text;
+		}
 		return null;
 	}
 	
-	public void put(String text, Locale locale){
+	public void put(String text, Locale locale) throws SQLException, Exception{
+		if( get(locale) == null) {
+			PreparedStatement stmt = connection.prepareStatement("INSERT INTO texts (ref, locale, text) VALUES (?, ?, ?)");
+			stmt.setString(1, reference);
+			stmt.setString(2, locale.toLanguageTag());
+			stmt.setString(3, text);
+			stmt.execute();
+		} else {
+			PreparedStatement stmt = connection.prepareStatement("UPDATE texts SET text = ? WHERE ref = ? AND locale = ?");
+			stmt.setString(1, text);
+			stmt.setString(2, reference);
+			stmt.setString(3, locale.toLanguageTag());
+			stmt.execute();
+		}
 	}
 
 	@Override
