0408e9034d3dc7e244e2989a42aad98e63ffd820
Jörgen Rosén <joergen.rosen@gmail.com>
2012-07-08 23:30:11 +0200

    Cleaning up GUI code and adding open section.

--- Presentation-Generator/src/gui/DaxploreGUI.java
+++ Presentation-Generator/src/gui/DaxploreGUI.java
@@ -1,5 +1,7 @@
 package gui;
 
+import gui.controller.OpenController;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -12,6 +14,7 @@ import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JInternalFrame;
 import javax.swing.JDesktopPane;
 import javax.swing.JTabbedPane;
@@ -47,21 +50,35 @@ import javax.swing.JTextField;
 import javax.swing.JSeparator;
 import javax.swing.border.MatteBorder;
 import java.awt.Color;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+
+
+import java.awt.Toolkit;
+import java.io.File;
+import javax.swing.JTextPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
 
 /**
  * Main window handler class. Initialization of application goes here.
  */
 public class DaxploreGUI {
 
-	private JFrame frame;
+	private JFrame frmDaxploreProducer;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	private JTextField textField;
+	private JTable tableSPSS;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		
+		// use an appropriate look and feel here.
 		try {
 		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
 		        if ("Nimbus".equals(info.getName())) {
@@ -77,7 +94,7 @@ public class DaxploreGUI {
 			public void run() {
 				try {
 					DaxploreGUI window = new DaxploreGUI();
-					window.frame.setVisible(true);
+					window.frmDaxploreProducer.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -96,128 +113,231 @@ public class DaxploreGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 728, 637);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.WEST);
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		frmDaxploreProducer = new JFrame();
+		frmDaxploreProducer.setIconImage(Toolkit.getDefaultToolkit().getImage(DaxploreGUI.class.getResource("/gui/resources/Colorful_Chart_Icon_vol2.png")));
+		frmDaxploreProducer.setTitle("Daxplore Producer Developer Version");
+		frmDaxploreProducer.setResizable(false);
+		frmDaxploreProducer.setBounds(100, 100, 864, 732);
+		frmDaxploreProducer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDaxploreProducer.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JPanel mainPanel = new JPanel();
+		JPanel buttonPanel = new JPanel();
+		frmDaxploreProducer.getContentPane().add(buttonPanel, BorderLayout.WEST);
+		buttonPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		final JPanel mainPanel = new JPanel();
 		mainPanel.setBorder(new MatteBorder(0, 1, 0, 0, (Color) Color.GRAY));
-		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+		frmDaxploreProducer.getContentPane().add(mainPanel, BorderLayout.CENTER);
 		mainPanel.setLayout(new CardLayout(0, 0));
 		
 		final JPanel openPanel = new JPanel();
-		mainPanel.add(openPanel, "name_1340892548506154000");
+		mainPanel.add(openPanel, "openPanel");
 		openPanel.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Open file...");
-		btnNewButton.setBounds(23, 24, 94, 27);
-		openPanel.add(btnNewButton);
+		final JButton openFileButton = new JButton("Open file...");
+		openFileButton.setToolTipText("Opens an SPSS file");
+		openFileButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == openFileButton) {
+					JFileChooser fc = new JFileChooser();
+					FileNameExtensionFilter filter = new FileNameExtensionFilter(
+					        "SPSS Files", "spss");
+					    fc.setFileFilter(filter);
+			        int returnVal = fc.showOpenDialog(frmDaxploreProducer);
+
+			        if (returnVal == JFileChooser.APPROVE_OPTION) {
+			           File file = fc.getSelectedFile();
+			           //This is where a real application would open the file.
+			           System.out.println("Opening: " + file.getName() + ".");
+			           OpenController.ReadSPSSFile(fc);
+			        } else {
+			           System.out.println("Open command cancelled by user.");
+			        }
+				}
+			}
+		});
+		openFileButton.setBounds(6, 24, 150, 27);
+		openPanel.add(openFileButton);
 		
-		JButton btnNewButton_1 = new JButton("Create new file...");
-		btnNewButton_1.setBounds(129, 24, 162, 27);
-		openPanel.add(btnNewButton_1);
+		JButton createNewFileButton = new JButton("Create new file...");
+		createNewFileButton.setToolTipText("Creates a new SPSS file");
+		createNewFileButton.setBounds(168, 24, 150, 27);
+		openPanel.add(createNewFileButton);
 		
-		textField = new JTextField();
-		textField.setBounds(50, 81, 122, 27);
-		openPanel.add(textField);
-		textField.setColumns(10);
+		JLabel fileNameLabel = new JLabel("Filename:");
+		fileNameLabel.setBounds(16, 88, 71, 15);
+		openPanel.add(fileNameLabel);
 		
-		JLabel lblNewLabel_2 = new JLabel("File");
-		lblNewLabel_2.setBounds(23, 87, 57, 15);
-		openPanel.add(lblNewLabel_2);
+		JTextPane fileNameText = new JTextPane();
+		fileNameText.setEditable(false);
+		fileNameText.setBounds(112, 79, 343, 24);
+		openPanel.add(fileNameText);
+		
+		JLabel lastImportedLabel = new JLabel("Last imported:");
+		lastImportedLabel.setBounds(15, 128, 85, 16);
+		openPanel.add(lastImportedLabel);
+		
+		JTextPane lastImportedText = new JTextPane();
+		lastImportedText.setEditable(false);
+		lastImportedText.setBounds(112, 120, 343, 24);
+		openPanel.add(lastImportedText);
+		
+		// JTable test data.
+		String[] columnNames = {"First Name",
+                "Last Name",
+                "Sport",
+                "# of Years",
+                "Vegetarian"};
+		
+		Object[][] data = {
+			    {"Kathy", "Smith",
+			     "Snowboarding", new Integer(5), new Boolean(false)},
+			    {"John", "Doe",
+			     "Rowing", new Integer(3), new Boolean(true)},
+			    {"Sue", "Black",
+			     "Knitting", new Integer(2), new Boolean(false)},
+			    {"Jane", "White",
+			     "Speed reading", new Integer(20), new Boolean(true)},
+			    {"Joe", "Brown",
+			     "Pool", new Integer(10), new Boolean(false)}
+			};
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 173, 761, 520);
+		openPanel.add(scrollPane);
+		
+		tableSPSS = new JTable(data, columnNames);
+		scrollPane.setViewportView(tableSPSS);
+		
+		JSeparator openSeparator = new JSeparator();
+		openSeparator.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		openSeparator.setBounds(6, 67, 761, 94);
+		openPanel.add(openSeparator);
 		
 		final JPanel importPanel = new JPanel();
-		mainPanel.add(importPanel, "name_1340892556990884000");
+		mainPanel.add(importPanel, "importPanel");
+		importPanel.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("Import");
+		lblNewLabel_1.setBounds(389, 5, 38, 16);
 		importPanel.add(lblNewLabel_1);
 		
 		JPanel editPanel = new JPanel();
-		mainPanel.add(editPanel, "name_1340892590961725000");
-		editPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		mainPanel.add(editPanel, "editPanel");
+		editPanel.setLayout(null);
 		
 		JLabel lblEdit = new JLabel("Edit");
+		lblEdit.setBounds(397, 5, 42, 16);
 		editPanel.add(lblEdit);
 		
 		JPanel sortPanel = new JPanel();
-		mainPanel.add(sortPanel, "name_1340892592836118000");
-		sortPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		mainPanel.add(sortPanel, "sortPanel");
+		sortPanel.setLayout(null);
 		
 		JLabel lblNewLabel_4 = new JLabel("Sort data");
+		lblNewLabel_4.setBounds(381, 5, 75, 16);
 		sortPanel.add(lblNewLabel_4);
 		
 		JPanel packagePanel = new JPanel();
-		mainPanel.add(packagePanel, "name_1340892595957519000");
+		mainPanel.add(packagePanel, "packagePanel");
+		packagePanel.setLayout(null);
 		
 		JLabel lblNewLabel_5 = new JLabel("Package");
+		lblNewLabel_5.setBounds(384, 5, 71, 16);
 		packagePanel.add(lblNewLabel_5);
 		
 		JPanel questionsPanel = new JPanel();
-		mainPanel.add(questionsPanel, "name_1341574307100846000");
+		mainPanel.add(questionsPanel, "questionsPanel");
+		questionsPanel.setLayout(null);
 		
 		JLabel questionsLabel = new JLabel("Questions");
+		questionsLabel.setBounds(380, 5, 74, 16);
 		questionsPanel.add(questionsLabel);
 		
-		// button control section
+		// radio button control section
 		JRadioButton btnOpen = new JRadioButton("");
+		btnOpen.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				CardLayout cl = (CardLayout)(mainPanel.getLayout());
+			    cl.show(mainPanel, "openPanel");
+			}
+		});
 		btnOpen.setRolloverEnabled(false);
 		btnOpen.setSelectedIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/8_selected.png")));
 		btnOpen.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		buttonGroup.add(btnOpen);
 		btnOpen.setIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/8.png")));
-		panel.add(btnOpen);
+		buttonPanel.add(btnOpen);
 		
 		JRadioButton btnImport = new JRadioButton("");
+		btnImport.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				CardLayout cl = (CardLayout)(mainPanel.getLayout());
+			    cl.show(mainPanel, "importPanel");
+			}
+		});
 		btnImport.setSelectedIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/4_selected.png")));
 		btnImport.setRolloverEnabled(false);
 		btnImport.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		buttonGroup.add(btnImport);
 		btnImport.setIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/4.png")));
-		panel.add(btnImport);
+		buttonPanel.add(btnImport);
 
 		JRadioButton btnQuestions = new JRadioButton("");
+		btnQuestions.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				CardLayout cl = (CardLayout)(mainPanel.getLayout());
+			    cl.show(mainPanel, "questionsPanel");
+			}
+		});
 		btnQuestions.setSelectedIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/6_selected.png")));
 		btnQuestions.setRolloverEnabled(false);
 		btnQuestions.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		buttonGroup.add(btnQuestions);
 		btnQuestions.setIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/6.png")));
-		panel.add(btnQuestions);
+		buttonPanel.add(btnQuestions);
 	
-		JRadioButton btnEditor = new JRadioButton("");
-		btnEditor.setSelectedIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/21_selected.png")));
-		btnEditor.setRolloverEnabled(false);
-		btnEditor.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		buttonGroup.add(btnEditor);
-		btnEditor.setIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/21.png")));
-		panel.add(btnEditor);
+		JRadioButton btnEdit = new JRadioButton("");
+		btnEdit.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				CardLayout cl = (CardLayout)(mainPanel.getLayout());
+			    cl.show(mainPanel, "editPanel");
+			}
+		});
+		btnEdit.setSelectedIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/21_selected.png")));
+		btnEdit.setRolloverEnabled(false);
+		btnEdit.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		buttonGroup.add(btnEdit);
+		btnEdit.setIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/21.png")));
+		buttonPanel.add(btnEdit);
 		
 		JRadioButton btnSort = new JRadioButton("");
+		btnSort.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				CardLayout cl = (CardLayout)(mainPanel.getLayout());
+			    cl.show(mainPanel, "sortPanel");
+			}
+		});
 		btnSort.setSelectedIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/24_selected.png")));
 		btnSort.setRolloverEnabled(false);
 		btnSort.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		buttonGroup.add(btnSort);
 		btnSort.setIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/24.png")));
-		panel.add(btnSort);
+		buttonPanel.add(btnSort);
 		
 		JRadioButton btnPackage = new JRadioButton("");
+		btnPackage.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				CardLayout cl = (CardLayout)(mainPanel.getLayout());
+			    cl.show(mainPanel, "packagePanel");
+			}
+		});
 		btnPackage.setSelectedIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/28_selected.png")));
 		btnPackage.setRolloverEnabled(false);
 		btnPackage.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		buttonGroup.add(btnPackage);
 		btnPackage.setIcon(new ImageIcon(DaxploreGUI.class.getResource("/gui/resources/28.png")));
-		panel.add(btnPackage);
-	}
-	private class SwingAction extends AbstractAction {
-		public SwingAction() {
-			putValue(NAME, "SwingAction");
-			putValue(SHORT_DESCRIPTION, "Some short description");
-		}
-		public void actionPerformed(ActionEvent e) {
-		}
+		buttonPanel.add(btnPackage);
 	}
 }
