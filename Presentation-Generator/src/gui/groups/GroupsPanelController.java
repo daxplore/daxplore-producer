c70a82759ced98f5de66836b58f40cd306f0cb72
Jörgen Rosén <joergen.rosen@gmail.com>
2012-08-29 20:44:51 +0200

    Daily commit.

--- Presentation-Generator/src/gui/groups/GroupsPanelController.java
+++ Presentation-Generator/src/gui/groups/GroupsPanelController.java
@@ -1,7 +1,15 @@
 package gui.groups;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.TransferHandler;
+import javax.swing.TransferHandler.TransferSupport;
 
 public class GroupsPanelController implements ActionListener {
 
@@ -9,6 +17,56 @@ public class GroupsPanelController implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public boolean importData(TransferHandler.TransferSupport info) {
+        String data = null;
+
+        //If we cannot handle the import, bail now.
+        if (!canImport(info)) {
+            return false;
+        }
+
+        JList list = (JList)info.getComponent();
+        DefaultListModel model = (DefaultListModel)list.getModel();
+        //Fetch the data -- bail if this fails
+        try {
+            data = (String)info.getTransferable().getTransferData(DataFlavor.stringFlavor);
+        } catch (UnsupportedFlavorException ufe) {
+            System.out.println("importData: unsupported data flavor");
+            return false;
+        } catch (IOException ioe) {
+            System.out.println("importData: I/O exception");
+            return false;
+        }
+
+        if (info.isDrop()) { //This is a drop
+            JList.DropLocation dl = (JList.DropLocation)info.getDropLocation();
+            int index = dl.getIndex();
+            if (dl.isInsert()) {
+                model.add(index, data);
+                return true;
+            } else {
+                model.set(index, data);
+                return true;
+            }
+        } else { //This is a paste
+            int index = list.getSelectedIndex();
+            // if there is a valid selection,
+            // insert data after the selection
+            if (index >= 0) {
+                model.add(list.getSelectedIndex()+1, data);
+            // else append to the end of the list
+            } else {
+                model.addElement(data);
+            }
+            return true;
+        }
+    }
+
+	private boolean canImport(TransferSupport info) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- Presentation-Generator/src/gui/groups/GroupsPanelView.java
+++ Presentation-Generator/src/gui/groups/GroupsPanelView.java
@@ -7,16 +7,64 @@ import javax.swing.JPanel;
 
 import gui.GuiMain;
 import gui.GuiFile;
+import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.BevelBorder;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JList;
+import javax.swing.JButton;
 
 public class GroupsPanelView extends JPanel {
 	
 	private JLabel lblGroupsPanel = new JLabel();
+	private JTable table;
+	private JTable table_1;
+	private JTable table_2;
 	
 	public GroupsPanelView(GuiMain guiMain) {
 		
-		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel questionsPanel = new JPanel();
+		questionsPanel.setBounds(6, 6, 326, 729);
+		questionsPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		
-		JLabel lblGroupPanel = new JLabel("Group Panel");
-		add(lblGroupPanel);
+		JPanel groupsPanel = new JPanel();
+		groupsPanel.setBounds(344, 6, 354, 321);
+		groupsPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		
+		JPanel editPanel = new JPanel();
+		editPanel.setBounds(344, 339, 354, 396);
+		editPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		
+		JButton btnNewButton = new JButton("Edit");
+		btnNewButton.setBounds(725, 6, 49, 28);
+		
+		JLabel lblGroupFlow = new JLabel("No file loaded");
+		editPanel.add(lblGroupFlow);
+		
+		JLabel lblEditField = new JLabel("No file loaded");
+		groupsPanel.add(lblEditField);
+		setLayout(null);
+		add(btnNewButton);
+		add(questionsPanel);
+		
+		JLabel lblNewLabel = new JLabel("No file loaded");
+		questionsPanel.add(lblNewLabel);
+		
+		table = new JTable();
+		questionsPanel.add(table);
+		add(editPanel);
+		
+		table_2 = new JTable();
+		editPanel.add(table_2);
+		add(groupsPanel);
+		
+		table_1 = new JTable();
+		groupsPanel.add(table_1);
 	}
 }
