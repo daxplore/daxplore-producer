212e916ecc589e57b4a31ca5471d9625984fded0
Daniel Dunér <daniel.duner@gmail.com>
2013-11-15 16:43:12 +0100

    Add a 5px empty border around the section headers

--- src/org/daxplore/producer/gui/SectionHeader.java
+++ src/org/daxplore/producer/gui/SectionHeader.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import org.daxplore.producer.gui.resources.GuiTexts;
 
@@ -11,6 +12,7 @@ import org.daxplore.producer.gui.resources.GuiTexts;
 public class SectionHeader extends JPanel {
 	public SectionHeader(GuiTexts texts, String name) {
 		setLayout(new BorderLayout());
+		setBorder(new EmptyBorder(5, 5, 5, 5));
 		String header = "<html><b>" + texts.get("header." + name + ".title") + "</b></html>";
 		String explanation = texts.get("header." + name + ".explanation"); 
 		add(new JLabel(header), BorderLayout.NORTH);
