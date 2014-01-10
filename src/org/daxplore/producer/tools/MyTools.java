5a4b7adc468b3a8f5fec6b19d60c07eabd994ffa
Daniel Dunér <daniel.duner@gmail.com>
2014-01-10 14:28:35 +0100

    Add GPL v2. license to project

--- src/org/daxplore/producer/tools/MyTools.java
+++ src/org/daxplore/producer/tools/MyTools.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2013-2014 Daniel Dunér, Axel Winkler.
+ * All rights reserved. This program is free software: it is made
+ * available under the terms of the GNU Public License v2.0 (or later)
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
 package org.daxplore.producer.tools;
 
 import java.sql.SQLException;
